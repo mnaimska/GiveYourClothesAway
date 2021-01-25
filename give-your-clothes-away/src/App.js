31b4facdeac1a0d586d7382fa2366843e0e4399d
MNAIMSKA <mnaimska@gmail.com>
2021-01-25 18:42:14 +0100

    componentsStructure

--- give-your-clothes-away/src/App.js
+++ give-your-clothes-away/src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import './scss/components/App.scss';
 import Home from './components/Home/Home';
 import FormStep1Header from "./components/Form/Form-Step1/FormStep1Header";
+import Register from "./components/Register";
 function App() {
   return (
    <BrowserRouter>
        <Switch>
            <Route exact path="/" component={Home}/>
             <Route path="/oddaj-rzeczy" component={FormStep1Header}/>
+            <Route path="/zarejestruj" component={Register}/>
        </Switch>
    </BrowserRouter>
   );
---
+++ give-your-clothes-away/src/components/Register.js
@@ -0,0 +1,21 @@
+import React from "react";
+const Register =()=>{
+    return (
+        <>
+            <div className="top-menu">
+                <div className="login-register">
+                    <button className="button">Zaloguj</button>
+                    <button className="button">Załóż konto</button>
+                </div>
+                <div className="navigation">
+                    <h3>Start</h3>
+                    <h3>O co chodzi? </h3>
+                    <h3>O nas</h3>
+                    <h3>Fundacja i organizacje</h3>
+                    <h3>Kontakt</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Register;
