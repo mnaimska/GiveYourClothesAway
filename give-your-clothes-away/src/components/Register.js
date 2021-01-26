07fef2facdd9612d719df608499f7174f96fdace
MNAIMSKA <mnaimska@gmail.com>
2021-01-26 23:18:46 +0100

    Register_CSS

--- give-your-clothes-away/src/components/Register.js
+++ give-your-clothes-away/src/components/Register.js
@@ -15,6 +15,30 @@ const Register =()=>{
                     <h3>Kontakt</h3>
                 </div>
             </div>
+            <div className="center">
+            <p className="title">Załóż konto
+                <div className="decoration"></div>
+                <div className="form-box">
+                    <label className="label">Email</label>
+                    <input className="input" type="text" placeholder="email"/>
+                    <label className="label">Hasło</label>
+                    <input className="input" type="password" placeholder="hasło"/>
+                    <label className="label">Powtórz hasło</label>
+                    <input className="input" type="password" placeholder="powtórz hasło"/>
+
+
+                </div>
+            </p>
+
+            </div>
+            <div className="center buttons">
+            <button className="button">Zaloguj się</button>
+            <button className="button">Załóż konto</button>
+            </div>
+
+
+
+
         </>
     )
 }
